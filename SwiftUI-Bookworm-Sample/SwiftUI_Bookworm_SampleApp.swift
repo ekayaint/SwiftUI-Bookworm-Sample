7b168ad66cfa441ee75880682e22a8564cd03c0e
ekayaint <ekayaint@gmail.com>
2023-09-30 20:45:00 +0300

    Initial Commit

---
+++ SwiftUI-Bookworm-Sample/SwiftUI_Bookworm_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Bookworm_SampleApp.swift
+//  SwiftUI-Bookworm-Sample
+//
+//  Created by ekayaint on 30.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Bookworm_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
