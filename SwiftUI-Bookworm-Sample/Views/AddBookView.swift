4ae2127aa1faaff978317d1b52f078234e1ab737
ekayaint <ekayaint@gmail.com>
2023-10-03 21:12:46 +0300

    AddBookView is ok

--- SwiftUI-Bookworm-Sample/Views/AddBookView.swift
+++ SwiftUI-Bookworm-Sample/Views/AddBookView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct AddBookView: View {
     @Environment(\.managedObjectContext) var  moc
+    @Environment(\.dismiss) var dismiss
     
     @State private var title = ""
     @State private var author = ""
@@ -25,8 +26,41 @@ struct AddBookView: View {
                     TextField("Name of book", text: $title)
                     TextField("Author's name", text: $author)
                     
-                }
+                    Picker("Genre", selection: $genre) {
+                        ForEach(genres, id: \.self) {
+                            Text($0)
+                        }
+                    } //: Picker
+                } //: Sec
+                
+                Section {
+                    TextEditor(text: $review)
+                    Picker("Rating", selection: $rating) {
+                        ForEach(0..<6) {
+                            Text(String($0))
+                        }
+                    } //: picker
+                } header: {
+                    Text("Write a review")
+                } //: Sec
+                
+                Section {
+                    Button("Save") {
+                        let newBook = Book(context: moc)
+                        newBook.id = UUID()
+                        newBook.title = title
+                        newBook.author = author
+                        newBook.rating = Int16(rating)
+                        newBook.genre = genre
+                        newBook.review = review
+                        
+                        try? moc.save()
+                        dismiss()
+                    }
+                } //: Sec
+                
             } //: Form
+            .navigationTitle("Add Book")
         } //: Nav
     }
 }
--- SwiftUI-Bookworm-Sample/Views/ContentView.swift
+++ SwiftUI-Bookworm-Sample/Views/ContentView.swift
@@ -8,13 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: []) var books: FetchedResults<Book>
     
+    @State private var showingAddScreen = false
     
     var body: some View {
-        VStack {
-           Text("Hello")
-        }
-        .padding()
+        NavigationStack{
+            Text("Count: \(books.count)")
+                .navigationTitle("Bookworm")
+                .toolbar{
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button{
+                            showingAddScreen.toggle()
+                        } label: {
+                            Label("Add Book", systemImage: "plus")
+                        }
+                    } //: ToolbarItem
+                } //: toolbar
+                .sheet(isPresented: $showingAddScreen, content: {
+                    AddBookView()
+                })
+        } //: Nav
     }
 }
 
